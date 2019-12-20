58573afcd2320f2624b12885bc29f8ff8d30e2a5
i354864 <jinwei.xue@sap.com>
2019-12-20 17:42:34 +0800

    add the devServer to enable send Cross domain request

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
 module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://124.251.4.221/course/public/index.php/index',
+        ws: true,
+        changeOrigin: true
+      }
+    }
+  },
   lintOnSave: false
 }
