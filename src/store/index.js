4cf552b6debdbc80f5370ab43c335a1314fad2b6
Lin Yuhan <linyuhan@yuewen.com>
2019-12-31 14:16:16 +0800

    feat: user store

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import user from './modules/user'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  strict: process.env.NODE_ENV !== 'production',
+  state: {},
+  mutations: {},
+  actions: {},
   modules: {
+    user
   }
 })
