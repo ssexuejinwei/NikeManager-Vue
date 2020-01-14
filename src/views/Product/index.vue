0a93f0fa7c752e7c1a90148660fc5f4154071faf
giuem <giuemcom@gmail.com>
2020-01-14 20:27:34 +0800

    update

--- src/views/Product/index.vue
+++ src/views/Product/index.vue
@@ -3,44 +3,117 @@
     <header>
       <h1>商品管理</h1>
     </header>
-    <div class="list">
-      <p-card
-        class="list-item"
-        v-for="(product, id) in products"
-        :key="id"
-        :name="product.name"
-        :sell="product.sell"
-        :capacity="product.capacity"
-        :status="product.status"
-        :img="product.img"
+    <div>
+      <el-radio-group v-model="type">
+        <el-radio-button v-for="(value, key) in C_TYPES_TO_STR" :key="key" :label="key">{{value}}</el-radio-button>
+      </el-radio-group>
+    </div>
+    <el-table :data="products" @selection-change="handleSelect">
+      <el-table-column type="selection" width="55" />
+      <el-table-column label="商品名称" width="300">
+        <template slot-scope="scope">
+          <div class="product">
+            <img class="cover" :src="IMAGE_PREFIX + scope.row.coverimage">
+            <div class="info">
+              <div>ID: {{scope.row.id}}</div>
+              <div>所需积分: {{scope.row.current_price}}</div>
+            </div>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column label="库存/销量">
+        <template slot-scope="scope">
+          <span>{{ scope.row.store }} / {{ scope.row.sales }}</span>
+        </template></el-table-column>
+      <el-table-column label="创建时间" prop="create_time" width="160"></el-table-column>
+      <el-table-column v-if="type === '2' || type === '3' || type === '4'" label="发布时间" prop="putaway_time" width="160"></el-table-column>
+      <el-table-column v-if="type === '0'" label="商品状态">
+        <template slot-scope="scope">
+          <el-tag>{{ C_STATE_TO_STR[scope.row.state] || '未知' }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column v-else label="操作"></el-table-column>
+    </el-table>
+    <div class="table-bottom">
+      <div class="action">
+        <el-button v-if="type === '0'" type="primary">新增商品</el-button>
+        <el-button v-if="type === '1' || type === '2' || type === '4'" type="primary">批量下架</el-button>
+        <el-button v-if="type === '3'" type="primary">批量上架</el-button>
+        <el-button>批量删除</el-button>
+      </div>
+      <el-pagination
+        :page-count="total_page"
+        :current-page.sync="page"
+        layout="prev, pager, next"
       />
-      <router-link class="list-item" to="/product/add">
-        <p-card-add />
-      </router-link>
-      <!--占位-->
-      <i /><i /><i /><i /><i /><i /><i /><i /><i /><i /><i /><i /><i /><i />
     </div>
   </div>
 </template>
 
 <script>
-import _ from 'lodash';
-import PCard from './components/card';
-import PCardAdd from './components/card-add';
+import Axios from 'axios'
 
-const mockActivity = () => ({
-  id: 0,
-  name: 'Nike Air',
-  sell: 10,
-  capacity: 20,
-  status: _.sample(['出售中', '已售罄']),
-  img: ''
-})
+const C_TYPES_TO_STR = {
+  '0': '全部商品',
+  '1': '出售中商品',
+  '2': '预售商品',
+  '3': '未上架商品',
+  '4': '已售罄商品'
+}
+
+const C_STATE_TO_STR = {
+  '0': '出售中',
+  '1': '预售中',
+  '2': '未上架',
+  '3': '已售罄'
+}
+
 export default {
-  components: { PCard, PCardAdd },
+  // components: { PCard, PCardAdd },
   data() {
     return {
-      products: Array.from({length: 8}).map(() => mockActivity())
+      total_page: 1,
+      products: [],
+      selectedProducts: [],
+      type: '0',
+      page: 1,
+
+      IMAGE_PREFIX: process.env.VUE_APP_UPLOAD_PUBLIC_URL,
+      C_TYPES_TO_STR,
+      C_STATE_TO_STR,
+    }
+  },
+
+  created() {
+    this.fetchProduct()
+  },
+
+  watch: {
+    type() {
+      this.fetchProduct()
+    },
+
+    page() {
+      this.fetchProduct()
+    }
+  },
+
+  methods: {
+    fetchProduct() {
+      Axios.get('/sellerctr/getGoods', {
+        params: {
+          cur_page: this.page,
+          type: this.type
+        }
+      }).then(response => {
+        console.debug(response.data)
+        this.products = response.data.data.data
+        this.total_page = response.data.data.total_pages
+      })
+    },
+
+    handleSelect(val) {
+      this.selectedProducts = val
     }
   }
 }
@@ -49,28 +122,29 @@ export default {
 <style lang="scss" scoped>
 @import './common';
 
-.list {
-  $gap: 1rem;
-
+.table-bottom {
+  margin-top: 24px;
   display: flex;
-  flex-wrap: wrap;
+  justify-content: space-between;
+}
 
-  justify-content: space-around;
+.product {
+  display: flex;
+  align-items: flex-start;
+  align-content: flex-start;
 
-  margin-left: -$gap;
-  margin-right: -$gap;
+  .cover {
+    width: 100px;
+    height: 100px;
+    object-fit: cover;
 
-  &-item {
-    width: 150px;
-    margin: 0 $gap $gap;
-
-    text-decoration: none;
-    color: initial;
+    margin-right: 16px;
   }
 
-  > i {
+  .info {
     width: 150px;
-    margin: 0 $gap $gap;
   }
 }
+
+
 </style>
