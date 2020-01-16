deb4931424d53d6ff6a0bb6e5bc03af7a8d1338d
giuem <giuemcom@gmail.com>
2020-01-16 22:24:34 +0800

    update

--- src/views/Order/index.vue
+++ src/views/Order/index.vue
@@ -1,39 +1,61 @@
 <template>
   <div>
-    <el-table :data="shownData" border>
-      <el-table-column prop="id" label="编号" />
-      <el-table-column prop="created" label="创建时间">
-        <template slot-scope="scope">
-          {{ format(scope.row.created) }}
-        </template>
-      </el-table-column>
-      <el-table-column prop="score" label="消耗积分" />
-      <el-table-column prop="address" label="送货地址" />
-      <el-table-column prop="express_number" label="物流单号" />
-      <el-table-column label="交易状态">
-        <template slot-scope="scope">
-          <el-tag style="margin-right: 0.5rem" type="info">待完成订单</el-tag>
-          <el-button size="small" :disabled="scope.row.state === 'complete'">完成订单</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <div class="table-action">
-      <router-link to="/order/add">
-        <el-button>添加新订单</el-button>
-      </router-link>
-      <router-link to="/order/service">
-        <el-button>售后</el-button>
-      </router-link>
-      <div>
-        <el-pagination
-          layout="total, prev, pager, next, jumper"
-          hide-on-single-page
-          :total="total"
-          :page-size="pageSize"
-          :current-page.sync="current"
-        />
+    <div class="content">
+      <el-menu class="menu" :default-active="state" @select="handleSelect">
+        <el-menu-item index="1">全部订单</el-menu-item>
+        <el-menu-item index="2">代发货订单</el-menu-item>
+        <el-menu-item index="3">待收货订单</el-menu-item>
+        <el-menu-item index="4">已完成订单</el-menu-item>
+        <el-menu-item index="5">退/换货订单</el-menu-item>
+        <el-menu-item index="6">历史订单</el-menu-item>
+      </el-menu>
+      <div class="table">
+        <el-table :data="data" border>
+          <el-table-column prop="id" label="编号" />
+          <el-table-column prop="created" label="创建时间">
+            <template slot-scope="scope">
+              {{ format(scope.row.created) }}
+            </template>
+          </el-table-column>
+          <el-table-column prop="score" label="消耗积分" />
+          <el-table-column prop="address" label="送货地址" />
+          <el-table-column prop="express_number" label="物流单号" />
+          <el-table-column label="交易状态">
+            <template slot-scope="scope">
+              <span v-if="scope.row.state === '3'">
+                交易成功
+              </span>
+              <span v-else-if="scope.row.state === '0'">
+                <el-tag>待发货</el-tag>
+              </span>
+              <span v-else-if="scope.row.state === '2'">
+                <el-tag type="success">确认收货</el-tag>
+              </span>
+              <span v-else>null</span>
+              <!-- <el-tag style="margin-right: 0.5rem" type="info">待完成订单</el-tag>
+              <el-button size="small" :disabled="scope.row.state === 'complete'">完成订单</el-button> -->
+            </template>
+          </el-table-column>
+        </el-table>
+        <div class="table-action">
+          <!-- <router-link to="/order/add">
+            <el-button>添加新订单</el-button>
+          </router-link>
+          <router-link to="/order/service">
+            <el-button>售后</el-button>
+          </router-link> -->
+          <div>
+            <el-pagination
+              layout="total, prev, next, jumper"
+              hide-on-single-page
+              :total="total"
+              :current-page.sync="current"
+            />
+          </div>
+        </div>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -48,21 +70,25 @@ export default {
       data: [],
       // 分页
       current: 1,
-      pageSize: 5,
+      // pageSize: 5,
+      state: '1',
 
-      isLoading: false
+      isLoading: false,
     }
   },
   created() {
     this.getOrders()
   },
+
+  watch: {
+    state() {
+      this.getOrders()
+    }
+  },
+
   computed: {
     total() {
       return this.data.length
-    },
-    shownData() {
-      const start = (this.current - 1) * this.pageSize;
-      return this.data.slice(start, start + this.pageSize)
     }
   },
   methods: {
@@ -76,8 +102,8 @@ export default {
         const { data } = await Axios.get('/sellerctr/getOrder', {
           params: {
             cur_page: this.current,
-            page_size: this.pageSize,
-            state: '',
+            // page_size: this.pageSize,
+            state: this.state,
           }
         })
 
@@ -91,23 +117,37 @@ export default {
         })
 
         this.data = data.data.map(d => ({
-          id: d.id,
+          ...d,
           created: new Date(d.create_time),
-          score: d.score,
           address: [d.address?.address?.region?.label, d.address?.name, d.address?.tel].join(' '),
-          express_number: d.express_number
         }))
       } catch(e) {
         console.error(e)
       } finally {
         this.isLoading = false
       }
+    },
+
+    handleSelect(key) {
+      this.state = key
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.content {
+  display: flex;
+
+  .menu {
+    margin-right: 16px;
+  }
+
+  .table {
+    flex: 1;
+  }
+}
+
 .table-action {
   margin-top: 2rem;
   display: flex;
