2238519ad7d8695bce18bc1636d9189091d2e5b8
Giuem <giuemcom@gmail.com>
2020-02-02 17:27:50 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import './http/axios'
 import * as Sentry from '@sentry/browser';
 import * as Integrations from '@sentry/integrations';
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   Sentry.init({
     dsn: 'https://6dad3df829d84022ac4b9f791969fc52@sentry.io/1889540',
     integrations: [new Integrations.Vue({Vue, attachProps: true})],
