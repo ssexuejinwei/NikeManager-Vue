05c363cca1aa72da170c1ceb8e6417831d08c898
giuem <giuemcom@gmail.com>
2020-02-09 21:14:59 +0800

    update

--- src/views/Product/index.vue
+++ src/views/Product/index.vue
@@ -57,7 +57,9 @@
     </el-table>
     <div class="table-bottom">
       <div class="action">
-        <el-button v-if="type === '0'" type="primary"><router-link to="/product/add">新增商品</router-link></el-button>
+        <router-link to="/product/add">
+          <el-button v-if="type === '0'" type="primary">新增商品</el-button>
+        </router-link>
         <el-button v-if="type === '1' || type === '2' || type === '4'" type="primary" :disabled="!selectedProducts.length" @click="downGoods">批量下架</el-button>
         <el-button v-if="type === '3'" type="primary" :disabled="!selectedProducts.length" @click="upGoods">批量上架</el-button>
         <el-button :disabled="!selectedProducts.length" @click="delGoods">批量删除</el-button>
