d438aafb73b2dd2177397b850f1e37d740ac39b4
giuem <giuemcom@gmail.com>
2020-02-10 12:17:45 +0800

    update layout

--- src/views/_layout.vue
+++ src/views/_layout.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <header>
+  <el-container style="height: 100vh">
+    <el-aside class="aside" :width="sideWidth">
+      <div class="logo">NIKE管理系统</div>
       <el-menu
-        mode="horizontal"
+        class="menu"
+        background-color="#303e58"
+        text-color="#fff"
         router
         :default-active="defaultActive"
       >
@@ -13,18 +16,28 @@
           {{menu.name}}
         </el-menu-item>
       </el-menu>
-    </header>
-    <section class="main">
-      <router-view></router-view>
-    </section>
-  </div>
+    </el-aside>
+    <el-container>
+      <el-header class="header">
+        <div class="collapse-button" :class="{ active: collapse }" @click="toggleCollapse">
+          <i class="el-icon-s-fold"></i>
+        </div>
+        <div v-show="collapse">
+          NIKE管理系统
+        </div>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 import _ from 'lodash'
 
 const menus = [
-  { name: '首页', path: '/' },
+  // { name: '首页', path: '/' },
   { name: '活动管理', path: '/activity' },
   { name: '商品管理', path: '/product' },
   { name: '订单管理', path: '/order' },
@@ -37,6 +50,7 @@ export default {
   data() {
     return {
       menus,
+      collapse: false
     }
   },
 
@@ -49,12 +63,71 @@ export default {
       const match = _.maxBy(matches, o => o.path.length)
 
       return match.path
+    },
+
+    sideWidth() {
+      return this.collapse ? '0' : '200px'
+    }
+  },
+
+  methods: {
+    toggleCollapse() {
+      this.collapse = !this.collapse
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.aside {
+  transition: width 200ms ease;
+}
+
+.logo {
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+
+  background: #222c40;
+  color: #fff;
+
+  font-weight: bold;
+}
+
+.menu {
+  height: calc(100vh - 60px);
+  border: none;
+}
+
+.header {
+  height: 60px;
+  line-height: 60px;
+  background: #303e58;
+  color: #fff;
+  padding-left: 0;
+
+  display: flex;
+}
+
+.collapse-button {
+  width: 60px;
+  height: 60px;
+  text-align: center;
+  transition: background 100ms, transform ease 250ms;
+  cursor: pointer;
+
+  margin-right: 8px;
+  // background: #303e58;
+  &:hover {
+    background: rgba(255,255,255,0.1)
+  }
+
+  &.active {
+    // background: rgba(255,255,255,0.05)
+    transform: rotate(180deg)
+  }
+}
+
 .main {
   margin-top: 16px;
 }
